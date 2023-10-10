025b4a2582b2d345f7eec379291bd55276ebe2e5
xtianbong <christianmbong45@gmail.com>
2023-10-10 19:17:02 -0400

    Added sound effects and colour changes. Other UI tweaks.

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@ import time
 import csv
 import datetime
 import os
+import pygame
 
 class GameApp:
     def __init__(self, root):
@@ -17,14 +18,13 @@ class GameApp:
         
         self.target_number = None
         self.game_over = False  # To track whether the game is over
+        self.flash_duration = 500  # 500 milliseconds (0.5 seconds)
         
         if not os.path.isfile('scores.csv'):
             self.create_scores_file()
 
         self.target_label = tk.Label(root, text="", font=("Arial", 36))
         self.target_label.pack()
-
-        
 
         self.button_frame = tk.Frame(root)
         self.button_frame.pack()
@@ -39,6 +39,8 @@ class GameApp:
 
         self.game_over_label = tk.Label(root, text="", font=("Arial", 20))
         self.high_score_label = tk.Label(root, text="", font=("Arial", 16))
+
+        pygame.mixer.init()  # Initialize the mixer for sound effects
 
         self.next_round()
         self.bind_keys_to_buttons()  # Call the function to bind keys to buttons
@@ -57,23 +59,46 @@ class GameApp:
             if self.target_number == pressed_number:
                 self.score += 100
                 self.score_label.config(text="Score: " + str(self.score))
-                self.next_round()  # Start a new round
+                self.flash_screen_and_labels("green")  # Flash green for correct answer
+                self.root.after(self.flash_duration, self.next_round)  # Start a new round after flashing
+                self.play_sound_effect("sfx/success/1.mp3")
             else:
                 if self.score > self.highest_score:
                     self.highest_score = self.score
-                    self.high_score_label.config(text="Highscore: " + str(self.highest_score))
-                self.score_label.config(text="Score: " + str(self.score))
-                self.save_score(self.score)
-                final_score = self.score  # Save the final score
-                self.score = 0
-                self.game_over = True
-                self.target_label.grid_forget()  # Hide the game screen
-                self.button_frame.grid_forget()
-                if final_score > self.highest_score:
-                    self.game_over_label.config(text="Game Over\nScore: " + str(final_score) + " (Best!)\nHighscore: " + str(self.highest_score) + "\nPress space to reset")
+                    self.flash_screen_and_labels("red")  # Flash red for wrong answer
                 else:
-                    self.game_over_label.config(text="Game Over\nScore: " + str(final_score) + "\nHighscore: " + str(self.highest_score) + "\nPress space to reset")
-                self.game_over_label.pack()
+                    self.save_score(self.score)
+                    final_score = self.score  # Save the final score
+                    self.score = 0
+                    self.game_over = True
+                    self.target_label.grid_forget()  # Hide the game screen
+                    self.button_frame.grid_forget()
+                    self.flash_screen_and_labels("red")  # Flash red for wrong answer
+                    if final_score > self.highest_score:
+                        self.game_over_label.config(text="Game Over\nScore: " + str(final_score) + " (Best!)\nHighscore: " + str(self.highest_score) + "\nPress space to reset", font=("Arial", 30))
+                        self.root.after(self.flash_duration, self.display_game_over)
+                    else:
+                        self.game_over_label.config(text="Game Over\nScore: " + str(final_score) + "\nHighscore: " + str(self.highest_score) + "\nPress space to reset", font=("Arial", 30))
+                        self.root.after(self.flash_duration, self.display_game_over)
+                    self.play_sound_effect("sfx/failure/1.mp3")
+
+    def flash_screen_and_labels(self, color):
+        # Flash the screen
+        self.root.configure(bg=color)
+        # Flash the text labels
+        self.target_label.configure(bg=color)
+        self.score_label.configure(bg=color)
+        self.high_score_label.configure(bg=color)
+        self.root.update()
+        if color == "green":
+            self.root.after(self.flash_duration, self.reset_background)
+    
+    def reset_background(self):
+        # Reset the background color to white
+        self.root.configure(bg="white")
+        self.target_label.configure(bg="white")
+        self.score_label.configure(bg="white")
+        self.high_score_label.configure(bg="white")
 
     def save_score(self, score):
         now = datetime.datetime.now()
@@ -100,15 +125,29 @@ class GameApp:
             writer = csv.writer(file)
             writer.writerow(["Date/Time", "Score"])
 
+    def display_game_over(self):
+        self.root.configure(bg="red")  # Set the background to red for game over
+        self.target_label.configure(bg="red")
+        self.score_label.configure(bg="red")
+        self.high_score_label.configure(bg="red")
+        self.game_over_label.pack()
+
     def reset_game(self, event):
-        if self.game_over and event.keysym == "space":
-            self.game_over = False
-            self.target_label.pack()  # Show the game screen
-            self.button_frame.pack()
-            self.game_over_label.pack_forget()  # Hide the game-over screen
-            self.next_round()
-            self.score = 0
-            self.score_label.config(text="Score: 0")
+        self.root.configure(bg="white")  # Revert the background to white
+        self.target_label.configure(bg="white")
+        self.score_label.configure(bg="white")
+        self.high_score_label.configure(bg="white")
+        self.game_over = False
+        self.target_label.pack()  # Show the game screen
+        self.button_frame.pack()
+        self.game_over_label.pack_forget()  # Hide the game-over screen
+        self.next_round()
+        self.score = 0
+        self.score_label.config(text="Score: 0")
+
+    def play_sound_effect(self, sound_file):
+        pygame.mixer.music.load(sound_file)
+        pygame.mixer.music.play()
 
 if __name__ == "__main__":
     root = tk.Tk()
