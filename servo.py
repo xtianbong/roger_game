afc3b69195285cab9c1475a11eda8490e802454a
xtianbong <christianmbong45@gmail.com>
2023-10-30 19:00:45 -0400

    Adding 360 spin function for servo

--- servo.py
+++ servo.py
@@ -2,11 +2,11 @@ import RPi.GPIO as GPIO
 import time
 
 # Set the GPIO mode and pin
-servo_pin = 22  # Use GPIO pin 22
+servo_pin = 22 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(servo_pin, GPIO.OUT)
 
-# Create a PWM object with a frequency of 50Hz (standard for servos)
+# Create a PWM object with a frequency of 50Hz
 pwm = GPIO.PWM(servo_pin, 50)
 
 # Function to set the angle of the servo
@@ -19,11 +19,13 @@ def full_spin():
     
     try:
         pwm.start(0)  # Start PWM with a duty cycle of 0 (servo at 0 degrees)
-        set_angle(359)
-
+        while True:
+            # Rotate the servo 360 degrees continuously
+            for angle in range(0, 361, 10):
+                set_angle(angle)
     except KeyboardInterrupt:
-        # Exit the program when Ctrl+C is pressed
         pwm.stop()
         GPIO.cleanup()
 
 full_spin()
+GPIO.cleanup()
