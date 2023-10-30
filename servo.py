ccb450a03eed6b447a6b1a61e0447c6b3bdc1efb
xtianbong <christianmbong45@gmail.com>
2023-10-30 18:54:26 -0400

    Adding 360 spin function for servo

--- servo.py
+++ servo.py
@@ -13,21 +13,20 @@ pwm = GPIO.PWM(servo_pin, 50)
 def set_angle(angle):
     duty_cycle = 2.5 + 10 * angle / 180  # Convert angle to duty cycle (2.5% to 12.5%)
     pwm.ChangeDutyCycle(duty_cycle)
-    time.sleep(1)  # Allow time for the servo to move
+    time.sleep(0.5)  # Allow time for the servo to move
 
-try:
-    pwm.start(0)  # Start PWM with a duty cycle of 0 (servo at 0 degrees)
-    while True:
-        # Rotate the servo from 0 to 180 degrees
-        for angle in range(0, 181, 10):
-            set_angle(angle)
-        # Rotate the servo back from 180 to 0 degrees
-        for angle in range(180, -1, -10):
-            set_angle(angle)
+def full_spin():
+    
+    try:
+        pwm.start(0)  # Start PWM with a duty cycle of 0 (servo at 0 degrees)
+        while True:
+            # Rotate the servo from 0 to 360 degrees
+            for angle in range(0, 360, 10):
+                set_angle(angle)
 
-except KeyboardInterrupt:
-    # Exit the program when Ctrl+C is pressed
-    pwm.stop()
-    GPIO.cleanup()
+    except KeyboardInterrupt:
+        # Exit the program when Ctrl+C is pressed
+        pwm.stop()
+        GPIO.cleanup()
 
-set_angle(90) #test
+full_spin()
